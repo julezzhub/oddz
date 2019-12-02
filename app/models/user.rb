90897fb10b4871156562ab1aacb4a8907eaaf10a
Julius Selchow <julius_selchow@hotmail.com>
2019-12-02 15:22:41 +0200

    avater uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
   validates_uniqueness_of :username, message: "Username exists already"
   validates :username, format: { with: /(?=.{6,20})[a-zA-Z0-9._]/, message: "Username must be 6-20 characters long and can only contain letters, numbers, . and _" }
 
+  mount_uploader :avatar, PhotoUploader
   monetize :balance_cents
 end
