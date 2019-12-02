a5385286cd4f4d7ec57f1d1cba48f52f0e57e539
Julius Hillebrand <hillebrandjulius@gmail.com>
2019-12-02 11:44:36 +0200

    Devise and Pundit Setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_friendship
+
+  validates_presence_of :username, :avatar
+  validates_uniqueness_of :username, message: "Username exists already"
+  validates :username, format: { with: /(?=.{6,20})[a-zA-Z0-9._]/, message: "Username must be 6-20 characters long and can only contain letters, numbers, . and _" }
 end
-
-
-
-
