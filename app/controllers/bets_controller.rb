cfcdb73c395a6a2e3847252fbefafecfaad97def
Julius Hillebrand <hillebrandjulius@gmail.com>
2019-12-02 14:31:00 +0200

    Further form commit

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -5,4 +5,11 @@ class BetsController < ApplicationController
 
   def create
   end
+
+  def set_time(days, hours, minutes)
+    days_in_sec = 86_400 * days
+    hours_in_sec = 3600 * hours
+    minutes_in_sec = 60 * minutes
+    Time.now + days_in_sec + hours_in_sec + minutes_in_sec
+  end
 end
