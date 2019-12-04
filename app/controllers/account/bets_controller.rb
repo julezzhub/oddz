c613cc89facecfb805ef2f08521251a19b9706a4
Julius Selchow <julius_selchow@hotmail.com>
2019-12-04 20:14:52 +0200

    created betting history

--- app/controllers/account/bets_controller.rb
+++ app/controllers/account/bets_controller.rb
@@ -4,6 +4,12 @@ class Account::BetsController < ApplicationController
 		super([:account, scope])
   end
 
+  def index
+    @all_bets = policy_scope(Bet).where(friend: current_user)
+    @current_bets = @all_bets.select { |bet| bet.end_time > Time.now }
+    @past_bets = @all_bets.select { |bet| bet.end_time < Time.now }
+  end
+
   def pending
     @bets = policy_scope(Bet).where(friend: current_user, status: nil)
   end
