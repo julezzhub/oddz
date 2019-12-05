8a45be4b645f927447d822cfbb89d761a199dc2e
Julius Selchow <julius_selchow@hotmail.com>
2019-12-05 16:54:57 +0200

    show no nil status bets

--- app/controllers/account/bets_controller.rb
+++ app/controllers/account/bets_controller.rb
@@ -6,8 +6,8 @@ class Account::BetsController < ApplicationController
 
   def index
     @all_bets = policy_scope(Bet).where(friend: current_user).or(Bet.where(user: current_user))
-    @current_bets = @all_bets.select { |bet| bet.end_time > Time.now }
-    @past_bets = @all_bets.select { |bet| bet.end_time < Time.now }
+    @current_bets = @all_bets.select { |bet| bet.end_time > Time.now && !bet.status.nil? }
+    @past_bets = @all_bets.select { |bet| bet.end_time < Time.now && !bet.status.nil? }
   end
 
   def pending
