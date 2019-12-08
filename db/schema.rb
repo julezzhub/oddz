ef62831c409750e6d5e43ccfbf2185ad63d2f5a5
Benjamin Wendelstadt <bwendelstadt@gmail.com>
2019-12-08 20:32:38 +0200

    home-page-updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_12_08_093909) do
+ActiveRecord::Schema.define(version: 2019_12_08_160511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_12_08_093909) do
     t.integer "premade_hours", default: 0
     t.integer "premade_days", default: 0
     t.integer "bet_expiration"
+    t.string "thumbnail"
     t.index ["friend_id"], name: "index_bets_on_friend_id"
     t.index ["user_id"], name: "index_bets_on_user_id"
     t.index ["winner_id"], name: "index_bets_on_winner_id"
