d2aa5c19aa97ade050bae25d6779f6f1bcc5b85c
Leopold von Schultzendorff <leopoldvonschultzendorff@gmail.com>
2019-12-11 13:46:05 +0200

    various changes and  cssing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,8 @@ import 'ion-rangeslider/css/ion.rangeSlider.min.css';
 import { initSliders } from './sliders';
 import { initAutocomplete } from '../plugins/autocomplete';
 import { setupPendingBets } from '../plugins/pending-bet-countdown';
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 initSliders()
 initAutocomplete()
 setupPendingBets()
-
